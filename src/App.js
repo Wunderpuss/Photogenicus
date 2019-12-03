2fe8841b96fc602481a9948a57ba4e0e5ab8fa4b
LBui <51882406+LBui@users.noreply.github.com>
2019-12-03 12:33:35 -0500

    Test Commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Component } from 'react';
+
+class App extends Component{
+    
+    render(){
+        return(
+            <div>
+                <button className='submit'>Click Me</button>
+            </div>
+        )
+    }
+}
+
+export default App; 
