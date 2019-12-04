ce20b4934ce78245400e0e54119c230b2ce975cd
EggsBenedict2123 <EggsBenedict2123@users.noreply.github.com>
2019-12-03 23:37:37 -0500

    front bois

--- src/client/index.jsx
+++ src/client/index.jsx
@@ -1,39 +1,51 @@
 import React from "react";
-import CodeMirror from 'react-codemirror';
+import CodeMirror from "react-codemirror";
 
-import 'codemirror/mode/javascript/javascript';
-import 'codemirror/lib/codemirror.css';
-// import 'codemirror/theme/material.css';
+import "codemirror/mode/javascript/javascript";
+import "codemirror/lib/codemirror.css";
 
 class App extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { code: "// Algo problem\n// Type below\n" };
+    this.state = {
+      code:
+        "// Algo problem:\n//create a function that adds 3 to 'n'\n// Type below\n\nfunction addThree(n) {\n\n}"
+    };
     this.updateCode = this.updateCode.bind(this);
   }
   updateCode(newCode) {
-    console.log(newCode);
     this.setState({ code: newCode });
   }
+
   render() {
     // value to options should fetch to the server and grab a random code question
     const options = {
       lineNumbers: true,
-      mode: 'javascript',
+      mode: "javascript"
     };
-    return (<div>
-      <CodeMirror id="code" value={this.state.code} onChange={this.updateCode} options={options} />
-      <button onClick={
-        () => {
-          const data = this.state;
-          fetch('/code', {
-            method: 'POST',
-            body: data
-          }).then(unparsed => unparsed.json).then(response => {
-          })
-        }
-      }>CLICK ME</button>
-    </div>);
+    return (
+      <div>
+        <CodeMirror
+          value={this.state.code}
+          onChange={this.updateCode}
+          options={options}
+        />
+        <button
+          onClick={() => {
+            const data = { ...this.state };
+            //console.log(eval(data));
+            // fetch("/code", {
+            //   method: "POST",
+            //   body: data
+            // })
+            //   .then(unparsed => unparsed.json)
+            //   .then(response => {});
+          }}
+        >
+          Submit Answer
+        </button>
+      </div>
+    );
   }
 }
 
